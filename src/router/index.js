af4657856c0b12833737f968d6829994cd1a4a95
Chris-Carty <69872982+Chris-Carty@users.noreply.github.com>
2020-12-31 14:52:57 +0000

    Requires Auth

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Team from "../views/Team.vue";
 import SignIn from "../views/SignInFlow/SignIn.vue";
 import Request from "../views/SignInFlow/Request.vue";
 import Recover from "../views/SignInFlow/Recover.vue";
+import * as netlifyIdentityWidget from "netlify-identity-widget";
 
 Vue.use(VueRouter);
 
@@ -13,11 +14,17 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: "/team",
     name: "team",
     component: Team,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: "/signin",
@@ -40,6 +47,19 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const currentUser = netlifyIdentityWidget.currentUser();
+  const requiresAuth = to.matched.some(record => {
+    return record.meta.requiresAuth
+  })
+
+  if (requiresAuth && !currentUser) {
+    next("signin");
+  } else {
+    next();
+  }
 });
 
 export default router;
