1f8088f8442331a52a679d8fae6ceb399f09bc61
Chris-Carty <69872982+Chris-Carty@users.noreply.github.com>
2021-01-02 14:55:20 +0000

    Sign In Flow

--- src/views/SignInFlow/Recover.vue
+++ src/views/SignInFlow/Recover.vue
@@ -1,11 +1,99 @@
 <template>
-  <div></div>
+  <div
+    class="container"
+    :class="{ 'light-background': !isDarkMode, 'dark-background': isDarkMode }"
+  >
+    <RequestAccount />
+    <div class="login">
+      <img src="@/assets/DCHQ.svg" v-show="isDarkMode" />
+      <img src="@/assets/DCHQ-dark.svg" v-show="!isDarkMode" />
+      <h4 :class="{ 'light-text': isDarkMode, 'dark-text': !isDarkMode }">
+        Recover Account
+      </h4>
+      <form @submit.prevent="onSubmit">
+        <input
+          type="email"
+          placeholder="Email"
+          :class="{ 'light-field': isDarkMode, 'dark-field': !isDarkMode }"
+          v-model="email"
+          required
+        />
+        <button>Send Email</button>
+      </form>
+      <router-link
+        to="/signin"
+        :class="{ 'light-link': isDarkMode, 'dark-link': !isDarkMode }"
+        >Already have an account? Sign in now</router-link
+      >
+      <ThemeSwitch />
+    </div>
+  </div>
 </template>
 
 <script>
+import { auth } from "@/main";
+import RequestAccount from "@/components/RequestAccount";
+import ThemeSwitch from "@/components/ThemeSwitch";
+
 export default {
   name: "Recover",
+  components: {
+    RequestAccount,
+    ThemeSwitch,
+  },
+  data() {
+    return {
+      email: null,
+      password: null,
+      hasText: false,
+      text: "",
+    };
+  },
+  computed: {
+    isDarkMode() {
+      return this.$store.getters.isDarkMode;
+    },
+  },
+  methods: {
+    onSubmit() {
+      const email = this.email;
+
+      auth
+        .requestPasswordRecovery(email)
+        .then(() => {
+          this.$router.push({
+            name: "signin",
+            params: {
+              userRecoveredAccount: true,
+              email: email,
+            },
+          });
+        })
+        .catch((error) => {
+          alert("Error: " + error);
+        });
+    },
+  },
+  mounted() {
+    const params = this.$route.params;
+
+    if (params.userLoggedOut) {
+      this.hasText = true;
+      this.text = "You have logged out!";
+    }
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+
+.login {
+  width: 400px;
+}
+</style>
--- src/views/SignInFlow/Request.vue
+++ src/views/SignInFlow/Request.vue
@@ -1,11 +1,82 @@
 <template>
-  <div></div>
+  <div
+    class="container"
+    :class="{ 'light-background': !isDarkMode, 'dark-background': isDarkMode }"
+  >
+    <div class="login">
+      <img src="@/assets/DCHQ.svg" v-show="isDarkMode" />
+      <img src="@/assets/DCHQ-dark.svg" v-show="!isDarkMode" />
+      <h4 :class="{ 'light-text': isDarkMode, 'dark-text': !isDarkMode }">
+        Request Account
+      </h4>
+      <form @submit.prevent="onSubmit">
+        <input
+          type="email"
+          placeholder="Email"
+          :class="{ 'light-field': isDarkMode, 'dark-field': !isDarkMode }"
+          v-model="email"
+          required
+        />
+        <button>Request Account</button>
+      </form>
+      <router-link
+        to="/signin"
+        :class="{ 'light-link': isDarkMode, 'dark-link': !isDarkMode }"
+        >Already have an account? Sign in now</router-link
+      >
+      <ThemeSwitch />
+    </div>
+  </div>
 </template>
 
 <script>
+import ThemeSwitch from "@/components/ThemeSwitch";
+
 export default {
   name: "Request",
+  components: {
+    ThemeSwitch,
+  },
+  data() {
+    return {
+      email: null,
+      password: null,
+      hasText: false,
+      text: "",
+    };
+  },
+  computed: {
+    isDarkMode() {
+      return this.$store.getters.isDarkMode;
+    },
+  },
+  methods: {
+    onSubmit() {
+      const email = this.email;
+
+      // Slack API logic
+    },
+  },
+  mounted() {
+    const params = this.$route.params;
+
+    if (params.userLoggedOut) {
+      this.hasText = true;
+      this.text = "You have logged out!";
+    }
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+
+.login {
+  width: 400px;
+}
+</style>
