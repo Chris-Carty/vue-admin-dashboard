7fbd56177a88756b73fb5150a6ee304a3f9a8c9f
Chris-Carty <69872982+Chris-Carty@users.noreply.github.com>
2021-01-02 16:20:47 +0000

    Apex Charts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,17 +10,77 @@
           <div ref="months" class="months" @click="toggleMonths">Months</div>
         </div>
       </div>
+      <apexchart
+        class="chart1"
+        width="100%"
+        height="500px"
+        type="area"
+        :options="chartOptions"
+        :series="series"
+      ></apexchart>
     </div>
   </div>
 </template>
 
 <script>
 import Header from "@/components/Header.vue";
+import VueApexCharts from "vue-apexcharts";
 
 export default {
   name: "home",
   components: {
     Header,
+    apexchart: VueApexCharts,
+  },
+  data() {
+    return {
+      chartOptions: {
+        colors: ["#14f1d9", "#7b42f6"],
+        legend: {
+          labels: {
+            colors: [this.$store.getters.isDarkMode ? "white" : "black"],
+          },
+          position: "top",
+        },
+        tooltip: {
+          theme: "dark",
+        },
+        grid: {
+          xaxis: {
+            lines: {
+              show: true,
+            },
+          },
+          yaxis: {
+            lines: {
+              show: false,
+            },
+          },
+        },
+        chart: {
+          id: "users",
+        },
+        xaxis: {
+          type: "datetime",
+        },
+      },
+      series: [
+        {
+          name: "active users",
+          data: [
+            [new Date("January 1, 2019"), 30],
+            [new Date("January 5, 2019"), 40],
+          ],
+        },
+        {
+          name: "new users",
+          data: [
+            [new Date("January 1, 2019"), 80],
+            [new Date("January 5, 2019"), 20],
+          ],
+        },
+      ],
+    };
   },
   methods: {
     toggleDays() {
